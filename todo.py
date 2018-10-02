ef1853c2b3719359f62bd2b971bc7d864ac655ac
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2018-10-02 16:53:53 +1000

    init

---
+++ todo.py
@@ -0,0 +1,17 @@
+import torch
+from config import config
+
+_config = config()
+
+
+def evaluate(golden_list, predict_list):
+    pass;
+
+
+def new_LSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
+    pass;
+
+
+def get_char_sequence(model, batch_char_index_matrices, batch_word_len_lists):
+    pass;
+
