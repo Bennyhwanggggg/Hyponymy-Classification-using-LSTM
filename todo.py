febdceed7a1ef65ccff53cd93b46d7c4dfdd51d2
Ricky <ricky.zheng2@gmail.com>
2018-10-09 16:54:34 +1100

    add original code for task 2

--- todo.py
+++ todo.py
@@ -131,7 +131,27 @@ def evaluate(golden_list, predict_list, debug_mode=False):
 
 
 def new_LSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
-  pass;
+	## original code from torch.nn._functions.rnn.LSTMCell
+    if input.is_cuda:
+        igates = F.linear(input, w_ih)
+        hgates = F.linear(hidden[0], w_hh)
+        state = fusedBackend.LSTMFused.apply
+        return state(igates, hgates, hidden[1]) if b_ih is None else state(igates, hgates, hidden[1], b_ih, b_hh)
+
+    hx, cx = hidden
+    gates = F.linear(input, w_ih, b_ih) + F.linear(hx, w_hh, b_hh)
+
+    ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+    ingate = F.sigmoid(ingate)
+    forgetgate = F.sigmoid(forgetgate)
+    cellgate = F.tanh(cellgate)
+    outgate = F.sigmoid(outgate)
+
+    cy = (forgetgate * cx) + (ingate * cellgate)
+    hy = outgate * F.tanh(cy)
+
+    return hy, cy
 
 
 def get_char_sequence(model, batch_char_index_matrices, batch_word_len_lists):
